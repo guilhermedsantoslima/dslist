423fc7fd4aa23064f0b52546dfe388bf7caace91
guilherme.lima <https://github.com/guilhermedsantoslima>
2023-07-12 22:30:40 -0300

    excluindo atributo

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -26,16 +26,7 @@ public class Game {
 	private String imgUrl;
 	private String shortDescription;
 	private String longDescription;
-	private String gameName;
 	
-	public String getGameName() {
-		return gameName;
-	}
-
-	public void setGameName(String gameName) {
-		this.gameName = gameName;
-	}
-
 	public Game() {
 		
 	}
